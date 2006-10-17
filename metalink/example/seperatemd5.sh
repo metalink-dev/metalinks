5c8c0f423ee101d3791d44f8ea90d292a68929be
bneijt <bneijt@gmail.com>
2006-10-17 11:26:48 +0000

    Seperate MD5 AWK script

---
+++ metalink/example/seperatemd5.sh
@@ -0,0 +1,6 @@
+
+#Bash/AWK script to create seperate metalink files per MD5SUM entry
+#A) You must have metalink in your PATH (try: metalink --help)
+#B) Usage: sh ./seperatemd5.sh MD5SUMS sfmirrors
+
+awk -- 'BEGIN{tmpfile="/tmp/metamd5tmp.tmp";}//{print($0) > tmpfile; filename=$2;gsub(/\./, "_", filename); filename=filename "_md5.metalink"; close(tmpfile); system("metalink --md5 " tmpfile " < '$2' > " filename)}' < $1
