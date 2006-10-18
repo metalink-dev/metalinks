3fcb12820c7913641e0662d0006537cbdf00f91a
bneijt <bneijt@gmail.com>
2006-10-18 15:02:18 +0000

    - Added items to the TODO list - Added metalinkslinks package files - Renamed the seperatemd5 to separatemd5

--- metalink/example/separatemd5.sh
+++ metalink/example/separatemd5.sh
@@ -1,6 +1,6 @@
 
-#Bash/AWK script to create seperate metalink files per MD5SUM entry
+#Bash/AWK script to create separate metalink files per MD5SUM entry
 #A) You must have metalink in your PATH (try: metalink --help)
-#B) Usage: sh ./seperatemd5.sh MD5SUMS sfmirrors
+#B) Usage: sh ./separatemd5.sh MD5SUMS sfmirrors
 
 awk -- 'BEGIN{tmpfile="/tmp/metamd5tmp.tmp";}//{print($0) > tmpfile; filename=$2;gsub(/\./, "_", filename); filename=filename "_md5.metalink"; close(tmpfile); system("metalink --md5 " tmpfile " < '$2' > " filename)}' < $1
