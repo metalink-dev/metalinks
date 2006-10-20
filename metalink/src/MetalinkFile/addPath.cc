6ac82194f9b97fae626d709d3a9b2ce0dd21cb07
bneijt <bneijt@gmail.com>
2006-10-20 16:29:23 +0000

    - Globals cleanup - Add path extension, cleanup - Added --baseurl option

--- metalink/src/MetalinkFile/addPath.cc
+++ metalink/src/MetalinkFile/addPath.cc
@@ -7,10 +7,11 @@ void MetalinkFile::addPath(std::string const &type, std::string const &value)
 
 void MetalinkFile::addPath(std::string const &type, std::string const &base, std::string const &file)
 {
+	//TODO Use boost_filesystem for portability here.
 	std::string cfile(file);	//Clean file
-	if(cfile[0] == '.' && cfile[1] == '/')
+	if(cfile[0] == '.' && cfile[1] == Globals::dirSep)
 		cfile = cfile.substr(1);	//The slash will be eaten below
-	if(cfile[0] == '/')
+	if(cfile[0] == Globals::dirSep)
 		cfile = cfile.substr(1);
 	
 	d_paths.push_back(std::make_pair(type, base + cfile));
