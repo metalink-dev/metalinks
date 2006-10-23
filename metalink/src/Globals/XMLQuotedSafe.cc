3a2f8dce32c7d819ad88560f700f5fe6a23ac247
bneijt <bneijt@gmail.com>
2006-10-23 22:04:47 +0000

    - Documentation update - Remove some obsolete files - Moved to version 0.2.1

--- metalink/src/Globals/XMLQuotedSafe.cc
+++ metalink/src/Globals/XMLQuotedSafe.cc
@@ -3,5 +3,38 @@
 std::string Globals::XMLQuotedSafe(std::string value)
 {
 	//TODO Quote thingies: " -> &quot;
-	return XMLSafe(value);
+	//Entities
+	char *froma[] = {
+		"&",
+		"\"",
+		0
+	};
+	
+	char *tob[] = {
+		"&amp;",
+		"&quot;"
+	};
+	
+	for(unsigned i(0); froma[i]; ++i)
+	{
+		string const from(froma[i]);
+		string const to(tob[i]);
+		string::size_type offset(0);
+		
+		while(true)
+		{
+			string::size_type pos = value.find(from, offset);
+			
+			if(pos == string::npos)
+				break;
+			cout << static_cast<int>(pos) << " o" << from.size() << " s" << to.size() << endl;
+			cout << " [" << value << "] ";
+			value = value.substr(0, pos) + to + value.substr(pos + from.size());
+			break;
+			cout << " [" << value << "] ";
+			offset += pos + to.size();
+break;
+		}
+	}
+	return value;
 }
--- metalink/src/Globals/genstylesheet.sh
+++
@@ -1,7 +0,0 @@
-
-#Generate the stylesheet variable from the monolithic XSLT file given as argument
-echo '#include "Globals.ih"' > stylesheet.cc
-echo "std::string Globals::stylesheet(\"\\" >> stylesheet.cc
-sed -e 's/"/\\"/g' -e 's/$/\\n\t\\/' < $1 >> stylesheet.cc
-echo '");' >> stylesheet.cc
-
