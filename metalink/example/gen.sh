99bf2141741f965db7cacb1ab606843dd2752896
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2006-10-30 10:48:00 +0000

    Updated examples

--- metalink/example/gen.sh
+++ metalink/example/gen.sh
@@ -4,4 +4,4 @@ metalink --alldigests metalinks/itworked.png < sfmirrors > itworked.metalink
 
 #Using MD5sum files
 md5sum *.* > MD5SUMS
-echo http http://example.com/|metalink --md5 MD5SUMS > frommd5.metalink
+metalink --md5 MD5SUMS < example.com > frommd5.metalink
