81a636015d62ff1c717a28b98396f2be818c430b
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2006-11-01 22:44:40 +0000

    Bugfixed --addPath, code cleanup, example addition

--- metalink/src/Metalink/from.cc
+++ metalink/src/Metalink/from.cc
@@ -2,34 +2,47 @@
 
 //TODO indentation
 std::string Metalink::from(std::vector< MetalinkFile > files, std::string const headerFile)
-		{
-			std::ostringstream out;
-			out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-			out << "<metalink version=\"3.0\" xmlns=\"http://www.metalinker.org/\" generator=\"http://metalinks.sourceforge.net/\">\n";
+{
+	std::ostringstream out;
+	//Root
+	out << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	out << "<metalink version=\"3.0\"\n"
+			<< "  xmlns=\"http://www.metalinker.org/\"\n"
+			<< "  generator=\"http://metalinks.sourceforge.net/\"\n"
+			<< "  >\n";
 			
+	//Header
 	if(headerFile.size() > 0)
 	{
 		ifstream file(headerFile.c_str());
 		file.unsetf(ios::skipws);
 		if(file.is_open())
+		{
 			copy(istream_iterator<char>(file),
 				istream_iterator<char>(),
 				ostream_iterator<char>(out)
 				);
+			out << "\n";
+		}
 		else
 		{
 			cerr << "Warning: Could not open header file: " << headerFile << "\n";
 			throw "Opening headerfile failed";
 		}
-	} 
- 	out << "\n<files>\n";
-			_foreach(file, files)
-			{
-				file->finalize();
-				out << *file;
-				out << "\n";
-			}
- 			out << "</files>\n";
-			out << "</metalink>\n";
-			return out.str();
+	}
+	
+	//Files section
+	if(files.size() > 0)
+	{
+	 	out << "<files>\n";
+		_foreach(file, files)
+		{
+			file->finalize();
+			out << *file;
+			out << "\n";
 		}
+ 		out << "</files>\n";
+	}
+	out << "</metalink>\n";
+	return out.str();
+}
