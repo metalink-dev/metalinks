42b69b3b4e6a66744d8c48b0c2ed852ee11e2f13
bneijt <bneijt@gmail.com>
2006-11-24 22:24:29 +0000

    ed2k small bugfix for non zero overflows (didn't occur, but maybe in the future)

--- metalink/src/Hash/HashPieces/update.cc
+++ metalink/src/Hash/HashPieces/update.cc
@@ -10,7 +10,7 @@ void HashPieces::update(char const *bytes, unsigned int numbytes)
 	assert(numbytes < d_size);
 
 	//Add bytes till d_count is equal to blockSize
-	if(numbytes + d_count < d_size)
+	if(numbytes + d_count <= d_size)
 	{
 		d_h->update(bytes, numbytes);
 		d_count += numbytes;
