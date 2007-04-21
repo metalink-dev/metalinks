223008ae928d3ff7552f6660096347a3488868b7
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2007-04-21 19:12:26 +0000

    Moving to autotools

--- metalink/package.sh
+++ metalink/package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-ccbuild --nodefargs --args "-O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" --addres src/ccResolutions makefile src/metalink.cc > Makefile.metalink
+# Makefile support has been dropped in favor of GNU autotools support
+#ccbuild --nodefargs --args "-O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" --addres src/ccResolutions makefile src/metalink.cc > Makefile.metalink
 ccbuild --nodefargs --args "-O2 -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" --addres src/ccResolutions aap src/metalink.cc > metalink.aap
 
