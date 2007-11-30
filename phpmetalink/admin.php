e162808f04ff0cdf9b57426ac3308a90b9eab072
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2007-11-30 15:41:51 +0000

    Added todo Some patches to the hashing system, should include PHP metalink library the next time.

--- phpmetalink/admin.php
+++ phpmetalink/admin.php
@@ -5,5 +5,16 @@ $files = mirrorFiles();
 
 foreach($files as $file)
 {
-	echo $file . ': <a href="hash.php?type=md5&amp;file='.$file.'">md5</a> <a href="hash.php?type=sha1&amp;file='.$file.'">sha1</a><br />';
+	echo $file . ': ';
+	if(hasMD5($file))
+		echo ' MD5 ';//.MD5For($file);
+	else
+		echo '<a href="hash.php?type=md5&amp;file='.$file.'">calculate md5</a>';
+
+	if(hasSHA($file))
+		echo ' SHA1 ';//.SHA1For($file);
+	else
+		echo '<a href="hash.php?type=sha1&amp;file='.$file.'">calculate sha1</a>';
+
+	echo '<br />';
 }
