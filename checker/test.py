2f41105e9e9d4b9036f45ee1ed2d62907d300416
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-01-21 06:31:38 +0000

    Added test cases and script

---
+++ checker/test.py
@@ -0,0 +1,32 @@
+import metalink
+import os
+
+filedir = "testcases/"
+filenames = os.listdir(filedir)
+
+outfile = os.path.join(os.getcwd(), "OOo_2.3.1_Win32Intel_install_en-US.exe")
+filesize = 112341981
+checksums = {"sha1": "2c2849d173b1a5e6f8a3dc986383ee897bf4364d"}
+
+for filename in filenames:
+    print "Running:", filename
+    try:
+        os.remove(outfile)
+    except: pass
+    
+    progress = metalink.ProgressBar(55)
+    results = metalink.download_metalink(filedir + filename, os.getcwd(), handler=progress.download_update)
+    progress.download_end()
+
+##    if outfile not in results:
+##        print "FAIL: Download failed"
+    if not os.access(outfile, os.F_OK):
+        print "FAIL: File does not exist"
+    elif os.stat(outfile).st_size != filesize:
+        print "FAIL: Wrong file size"
+    elif not metalink.verify_checksum(outfile, checksums):
+        print "FAIL: Bad file checksum"
+    else:
+        print "PASS"
+        
+    print "---------------------------"
