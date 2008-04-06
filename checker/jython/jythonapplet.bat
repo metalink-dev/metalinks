64ecb073ba61667ddfebe715fab24df9d8bf8f22
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-04-06 21:09:02 +0000

    Fixed bugs in console, added non-functional jython code for reference

---
+++ checker/jython/jythonapplet.bat
@@ -0,0 +1,1 @@
+c:\jython2.2.1\jythonc.bat --core --deep --jar MetalinkApplet.jar MetalinkApplet.py
--- checker/test.py
+++ checker/test.py
@@ -1,4 +1,5 @@
-import metalink
+import download
+import console
 import os
 
 filedir = "testcases/"
@@ -9,24 +10,25 @@ filesize = 112341981
 checksums = {"sha1": "2c2849d173b1a5e6f8a3dc986383ee897bf4364d"}
 
 for filename in filenames:
-    print "Running:", filename
-    try:
-        os.remove(outfile)
-    except: pass
-    
-    progress = metalink.ProgressBar(55)
-    results = metalink.download_metalink(filedir + filename, os.getcwd(), handler=progress.download_update)
-    progress.download_end()
-
-##    if outfile not in results:
-##        print "FAIL: Download failed"
-    if not os.access(outfile, os.F_OK):
-        print "FAIL: File does not exist"
-    elif os.stat(outfile).st_size != filesize:
-        print "FAIL: Wrong file size"
-    elif not metalink.verify_checksum(outfile, checksums):
-        print "FAIL: Bad file checksum"
-    else:
-        print "PASS"
+    if filename != ".svn":
+        print "Running:", filename
+        try:
+            os.remove(outfile)
+        except: pass
         
-    print "---------------------------"
+        progress = console.ProgressBar(55)
+        results = download.download_metalink(filedir + filename, os.getcwd(), handler=progress.download_update)
+        progress.download_end()
+
+    ##    if outfile not in results:
+    ##        print "FAIL: Download failed"
+        if not os.access(outfile, os.F_OK):
+            print "FAIL: File does not exist"
+        elif os.stat(outfile).st_size != filesize:
+            print "FAIL: Wrong file size"
+        elif not download.verify_checksum(outfile, checksums):
+            print "FAIL: Bad file checksum"
+        else:
+            print "PASS"
+            
+        print "---------------------------"
