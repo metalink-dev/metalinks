c798c3517994e2065f002930a1156b76292625db
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-04-07 01:02:23 +0000

    Added language support

--- checker/setup.py
+++ checker/setup.py
@@ -1,3 +1,6 @@
+import sys
+import os.path
+
 #main is first
 modules = ['console', 'xmlutils', 'download', 'checker']
 outputfile = "metalink.py"
@@ -51,4 +54,62 @@ def readfile(modulename, imports=False):
     filehandle.close()
     return filestring
 
-merge(header, modules, outputfile)
+def clean():    
+    filelist = []
+    filelist.extend(rec_search(".pyc"))
+    filelist.extend(rec_search(".pyo"))
+    
+    for filename in filelist:
+        try:
+            os.remove(filename)
+        except WindowsError: pass
+
+def localegen():
+    localedir = "locale"
+    ignore = ("setup.py", "test.py")
+
+    files = rec_search(".py")
+    for pyfile in files:
+        if os.path.basename(pyfile) not in ignore:
+            potdir = os.path.join(os.path.dirname(pyfile), localedir, os.path.basename(pyfile))[:-3]
+            print potdir
+            try:
+                os.makedirs(os.path.join(os.path.dirname(pyfile), localedir))
+            except: pass
+
+            command = os.path.join(sys.prefix, "Tools/i18n/pygettext.py") + " --no-location -d \"%s\" \"%s\"" % (potdir, pyfile)
+            print(command)
+            result = os.system(command)
+            if result != 0:
+                raise AssertionError, "Generation of .pot file failed for %s." % pyfile
+
+
+def localecompile():
+    files = rec_search(".po")
+
+    for pofile in files:
+        command = os.path.join(sys.prefix, "Tools/i18n/msgfmt.py") + " \"%s\"" % pofile
+        print(command)
+        result = os.system(command)
+        if result != 0:
+            raise AssertionError, "Generation of .mo file failed for %s." % pofile
+            
+def rec_search(end, abspath = True):
+    start = os.path.dirname(__file__)
+    mylist = []
+    for root, dirs, files in os.walk(start):
+        for filename in files:
+            if filename.endswith(end):
+                if abspath:
+                    mylist.append(os.path.join(root, filename))
+                else:
+                    mylist.append(os.path.join(root[len(start):], filename))
+                    
+    return mylist
+
+if sys.argv[1] == 'merge':
+    merge(header, modules, outputfile)
+
+elif sys.argv[1] == 'translate':
+    localegen()
+    localecompile()
