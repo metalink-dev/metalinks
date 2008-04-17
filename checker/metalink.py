76b964eb2222b46159a9c234560e567e9fe7bf24
nabber00 <nabber00@gmail.com>
2008-04-17 01:00:35 +0000

    New release with removal of pyme dependency

--- checker/metalink.py
+++ checker/metalink.py
@@ -34,7 +34,7 @@
 #
 # Instructions:
 #   1. You need to have Python installed.
-#   2. To check PGP signatures you need to install pyme (optional, http://pyme.sourceforge.net/)
+#   2. To check PGP signatures you need to install gpg or gpg4win (http://www.gpg4win.org/)
 #   3. Run on the command line using: python metalink.py
 #
 #   Usage: metalink.py [options]
@@ -53,6 +53,9 @@
 #     -k DIR, --pgp-keys=DIR
 #                           Directory with the PGP keys that you trust (default:
 #                           working directory)
+#     -p FILE, --pgp-store=FILE
+#                           File with the PGP keys that you trust (default:
+#                           ~/.gnupg/pubring.gpg)
 #
 # Library Instructions:
 #   - Use as expected.
@@ -63,6 +66,10 @@
 # results = metalink.check_metalink("file.metalink")
 #
 # CHANGELOG:
+#
+# Version 3.7.2
+# -------------
+# - Modified to remove the pyme dependency
 #
 # Version 3.7.1
 # -------------
@@ -180,13 +187,12 @@ import copy
 import socket
 import ftplib
 import httplib
-try:
-    import pyme.core
-    import pyme.constants
-except: pass
 import sys
 import locale
 import gettext
+import os, StringIO #, popen2
+import os.path
+import subprocess
 import xml.dom.minidom
 import optparse
 import socket
@@ -588,7 +594,12 @@ checker.translate = translate
 ########################################################################
 
 
-USER_AGENT = "Metalink Checker/3.7.1 +http://www.nabber.org/projects/"
+##try:
+##    import pyme.core
+##    import pyme.constants
+##except: pass
+
+USER_AGENT = "Metalink Checker/3.7.2 +http://www.nabber.org/projects/"
 
 SEGMENTED = True
 LIMIT_PER_HOST = 1
@@ -609,6 +620,7 @@ if len(lang) == 5:
 
 PGP_KEY_DIR=""
 PGP_KEY_EXTS = (".gpg", ".asc")
+PGP_KEY_STORE=None
 
 # Configure proxies (user and password optional)
 # HTTP_PROXY = http://user:password@myproxy:port
@@ -815,7 +827,7 @@ def download_metalink(src, path, force = False, handler = None):
     
     urllist = xmlutils.get_subnodes(dom2, ["metalink", "files", "file"])
     if len(urllist) == 0:
-        #print _("No urls to download file from.")
+        print _("No urls to download file from.")
         return False
 
     results = []
@@ -1124,7 +1136,7 @@ def verify_checksum(local_file, checksums={}):
     try:
         checksums["pgp"]
         return pgp_verify_sig(local_file, checksums["pgp"])
-    except (KeyError, AttributeError, NameError): pass
+    except (KeyError, AttributeError, ValueError, AssertionError): pass
     try:
         checksums["sha512"]
         if filehash(local_file, hashlib.sha512()) == checksums["sha512"].lower():
@@ -1170,6 +1182,38 @@ def verify_checksum(local_file, checksums={}):
     return True
 
 def pgp_verify_sig(filename, sig):
+    gpg = GPG.GPGSubprocess(keyring=PGP_KEY_STORE)
+
+    for root, dirs, files in os.walk(PGP_KEY_DIR):
+        for thisfile in files:
+            if thisfile[-4:] in PGP_KEY_EXTS:
+                gpg.import_key(open(thisfile).read())
+    
+    sign = gpg.verify_file_detached(filename, sig)
+
+    print "\n-----" + _("BEGIN PGP SIGNATURE INFORMATION") + "-----"
+    if sign.error != None:
+        print sign.error
+    else:
+        #print sig.creation_date
+        try:
+            print "" + _("timestamp") + ":", time.strftime("%a, %d %b %Y %H:%M:%S (%Z)", time.localtime(float(sign.timestamp)))
+        except TypeError: pass
+        print "" + _("fingerprint") + ":", sign.fingerprint
+        #print sig.signature_id
+        #print sign.key_id
+        print "" + _("uid") + ":", sign.username
+    print "-----" + _("END PGP SIGNATURE INFORMATION") + "-----\n"
+
+    if sign.error != None:
+        raise AssertionError, sign.error
+    
+    if sign.is_valid():
+        return True
+    
+    return False
+
+def old_pgp_verify_sig(filename, sig):
     c = pyme.core.Context()
 
     for root, dirs, files in os.walk(PGP_KEY_DIR):
@@ -1232,8 +1276,11 @@ def pgp_verify_sig(filename, sig):
         print "" + _("fingerprint") + ":", sign.fpr
         #print "  hash:", pyme.core.hash_algo_name(sign.hash_algo)
         #print "  sig algo:", pyme.core.pubkey_algo_name(sign.pubkey_algo)
-        print "" + _("uid") + ":", c.get_key(sign.fpr, 0).uids[0].uid
-
+        try:
+            print "" + _("uid") + ":", c.get_key(sign.fpr, 0).uids[0].uid
+        except:
+            print _("ERROR") + ": " + _("Could not find signing key.")
+        
         if sign.summary != 0 or sign.status != 0:
             retval = False
     print "-----" + _("END PGP SIGNATURE INFORMATION") + "-----\n"
@@ -1448,7 +1495,7 @@ class Segment_Manager:
             if len(self.urls) == 0:
                 self.close_handler()
                 return False
-            
+
         return False
 
     def update(self):
@@ -2232,6 +2279,7 @@ download.MAX_REDIRECTS = MAX_REDIRECTS
 download.OS = OS
 download.PGP_KEY_DIR = PGP_KEY_DIR
 download.PGP_KEY_EXTS = PGP_KEY_EXTS
+download.PGP_KEY_STORE = PGP_KEY_STORE
 download.PROTOCOLS = PROTOCOLS
 download.SEGMENTED = SEGMENTED
 download.Segment_Manager = Segment_Manager
@@ -2249,6 +2297,7 @@ download.get_transport = get_transport
 download.is_local = is_local
 download.is_remote = is_remote
 download.lang = lang
+download.old_pgp_verify_sig = old_pgp_verify_sig
 download.path_join = path_join
 download.pgp_verify_sig = pgp_verify_sig
 download.set_proxies = set_proxies
@@ -2259,6 +2308,380 @@ download.urlopen = urlopen
 download.urlretrieve = urlretrieve
 download.verify_checksum = verify_checksum
 download.verify_chunk_checksum = verify_chunk_checksum
+'''
+From sourceforge pycrypto project:
+http://sourceforge.net/projects/pycrypto/
+
+Code for running GnuPG from Python and dealing with the results.
+
+Detailed info about the format of data to/from gpg may be obtained from the
+file DETAILS in the gnupg source.
+'''
+
+__rcsid__ = '$Id: GPG.py,v 1.3 2003/11/23 15:03:15 akuchling Exp $'
+
+
+class Signature:
+    "Used to hold information about a signature result"
+
+    def __init__(self):
+        self.valid = 0
+        self.fingerprint = self.creation_date = self.timestamp = None
+        self.signature_id = self.key_id = None
+        self.username = None
+        self.error = None
+
+    def BADSIG(self, value):
+        self.valid = 0
+        self.key_id, self.username = value.split(None, 1)
+    def GOODSIG(self, value):
+        self.valid = 1
+        self.key_id, self.username = value.split(None, 1)
+    def VALIDSIG(self, value):
+        #print value
+        self.fingerprint, self.creation_date, self.timestamp, other = value.split(" ", 3)
+    def SIG_ID(self, value):
+        self.signature_id, self.creation_date, self.timestamp = value.split(" ", 2)
+    def NODATA(self, value):
+        self.error = "File not properly loaded for signature."
+    def ERRSIG(self, value):
+        #print value
+        self.error = "Signature error."
+    def NO_PUBKEY(self, value):
+        #print value
+        self.error = "Signature error, missing public key with id 0x%s." % value[-8:]
+    def TRUST_UNDEFINED(self, value):
+        pass
+        #print value.split()
+        #raise AssertionError, "File not properly loaded for signature."
+    
+    def is_valid(self):
+        return self.valid
+ 
+class ImportResult:
+    "Used to hold information about a key import result"
+
+    counts = '''count no_user_id imported imported_rsa unchanged
+            n_uids n_subk n_sigs n_revoc sec_read sec_imported
+            sec_dups not_imported'''.split()
+    def __init__(self):
+        self.imported = []
+        self.results = []
+        for result in self.counts:
+            setattr(self, result, None)
+    
+    def NODATA(self, value):
+        self.results.append({'fingerprint': None,
+            'problem': '0', 'text': 'No valid data found'})
+    def IMPORTED(self, value):
+        # this duplicates info we already see in import_ok and import_problem
+        pass
+    ok_reason = {
+        '0': 'Not actually changed',
+        '1': 'Entirely new key',
+        '2': 'New user IDs',
+        '4': 'New signatures',
+        '8': 'New subkeys',
+        '16': 'Contains private key',
+    }
+    def IMPORT_OK(self, value):
+        reason, fingerprint = value.split()
+        self.results.append({'fingerprint': fingerprint,
+            'ok': reason, 'text': self.ok_reason[reason]})
+    problem_reason = {
+        '0': 'No specific reason given',
+        '1': 'Invalid Certificate',
+        '2': 'Issuer Certificate missing',
+        '3': 'Certificate Chain too long',
+        '4': 'Error storing certificate',
+    }
+    def IMPORT_PROBLEM(self, value):
+        try:
+            reason, fingerprint = value.split()
+        except:
+            reason = value
+            fingerprint = '<unknown>'
+        self.results.append({'fingerprint': fingerprint,
+            'problem': reason, 'text': self.problem_reason[reason]})
+    def IMPORT_RES(self, value):
+        import_res = value.split()
+        for i in range(len(self.counts)):
+            setattr(self, self.counts[i], int(import_res[i]))
+
+    def summary(self):
+        l = []
+        l.append('%d imported'%self.imported)
+        if self.not_imported:
+            l.append('%d not imported'%self.not_imported)
+        return ', '.join(l)
+
+class ListResult:
+    ''' Parse a --list-keys output
+
+        Handle pub and uid (relating the latter to the former).
+
+        Don't care about (info from src/DETAILS):
+
+        crt = X.509 certificate
+        crs = X.509 certificate and private key available
+        sub = subkey (secondary key)
+        sec = secret key
+        ssb = secret subkey (secondary key)
+        uat = user attribute (same as user id except for field 10).
+        sig = signature
+        rev = revocation signature
+        fpr = fingerprint: (fingerprint is in field 10)
+        pkd = public key data (special field format, see below)
+        grp = reserved for gpgsm
+        rvk = revocation key
+    '''
+    def __init__(self):
+        self.pub_keys = []
+        self.pk = None
+
+    def pub(self, args):
+        keyid = args[4]
+        date = args[5]
+        uid = args[9]
+        self.pk = {'keyid': keyid, 'date': date, 'uids': [uid]}
+        self.pub_keys.append(self.pk)
+
+    def uid(self, args):
+        self.pk['uids'].append(args[9])
+
+class EncryptedMessage:
+    ''' Handle a --encrypt command
+    '''
+    def __init__(self):
+        self.data = ''
+
+    def BEGIN_ENCRYPTION(self, value):
+        pass
+    def END_ENCRYPTION(self, value):
+        pass
+
+class GPGSubprocess:
+
+    # Default path used for searching for the GPG binary, when the
+    # PATH environment variable isn't set.
+    #DEFAULT_PATH = ['/bin', '/usr/bin', '/usr/local/bin', '${PROGRAMFILES}\\GNU\\GnuPG', '${PROGRAMFILES(X86)}\\GNU\\GnuPG']
+    DEFAULT_PATH = []
+    
+    def __init__(self, gpg_binary=None, keyring=None):
+        """Initialize an object instance.  Options are:
+
+        gpg_binary -- full pathname for GPG binary.  If not supplied,
+        the current value of PATH will be searched, falling back to the
+        DEFAULT_PATH class variable if PATH isn't available.
+
+        keyring -- full pathname to the public keyring to use in place of
+        the default "~/.gnupg/pubring.gpg".
+        """
+        # If needed, look for the gpg binary along the path
+        if gpg_binary is None:
+            path = self.DEFAULT_PATH
+            if os.environ.has_key('PATH'):
+                temppath = os.environ['PATH']
+                path.extend(temppath.split(os.pathsep))
+            #else:
+            #    path = self.DEFAULT_PATH
+
+            for pathdir in path:
+                pathdir = os.path.expandvars(pathdir)
+                fullname = os.path.join(pathdir, 'gpg')
+                if os.path.exists(fullname):
+                    gpg_binary = fullname
+                    break
+
+                if os.path.exists(fullname + ".exe"):
+                    gpg_binary = fullname + ".exe"
+                    break
+            else:
+                raise ValueError, ("Couldn't find 'gpg' binary on path"
+                                   + repr(path) )
+
+        self.gpg_binary = "\"" + gpg_binary + "\""
+        self.keyring = keyring
+
+    def _open_subprocess(self, *args):
+        # Internal method: open a pipe to a GPG subprocess and return
+        # the file objects for communicating with it.
+        cmd = [self.gpg_binary, '--status-fd 2']
+        if self.keyring:
+            cmd.append('--keyring "%s" --no-default-keyring'%self.keyring)
+
+        cmd.extend(args)
+        cmd = ' '.join(cmd)
+
+        #print cmd
+
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #child_stdout, child_stdin, child_stderr =  #popen2.popen3(cmd)
+        #return child_stdout, child_stdin, child_stderr
+        #print process.stderr
+        return process.stdout, process.stdin, process.stderr
+
+    def _read_response(self, child_stdout, response):
+        # Internal method: reads all the output from GPG, taking notice
+        # only of lines that begin with the magic [GNUPG:] prefix.
+        # 
+        # Calls methods on the response object for each valid token found,
+        # with the arg being the remainder of the status line.
+        while 1:
+            line = child_stdout.readline()
+            #print line
+            if line == "": break
+            line = line.rstrip()
+            if line[0:9] == '[GNUPG:] ':
+                # Chop off the prefix
+                line = line[9:]
+                L = line.split(None, 1)
+                keyword = L[0]
+                if len(L) > 1:
+                    value = L[1]
+                else:
+                    value = ""
+                getattr(response, keyword)(value)
+
+    def _handle_gigo(self, args, file, result):
+        # Handle a basic data call - pass data to GPG, handle the output
+        # including status information. Garbage In, Garbage Out :)
+        child_stdout, child_stdin, child_stderr = self._open_subprocess(*args)
+
+        # Copy the file to the GPG subprocess
+        while 1:
+            data = file.read(1024)
+            if data == "": break
+            child_stdin.write(data)
+        child_stdin.close()
+
+        # Get the response information
+        resp = self._read_response(child_stderr, result)
+
+        # Read the contents of the file from GPG's stdout
+        result.data = ""
+        while 1:
+            data = child_stdout.read(1024)
+            if data == "": break
+            result.data = result.data + data
+
+        return result
+    
+
+    #
+    # SIGNATURE VERIFICATION METHODS
+    #
+    def verify(self, data):
+        "Verify the signature on the contents of the string 'data'"
+        file = StringIO.StringIO(data)
+        return self.verify_file(file)
+    
+    def verify_file(self, file):
+        "Verify the signature on the contents of the file-like object 'file'"
+        sig = Signature()
+        self._handle_gigo(['--verify -'], file, sig)
+        return sig
+
+    def verify_file_detached(self, filename, sigtext):
+        sig = Signature()
+        sigfile = StringIO.StringIO(sigtext)
+        self._handle_gigo(["--verify - \"%s\"" % filename], sigfile, sig)
+        return sig
+
+    #
+    # KEY MANAGEMENT
+    #
+    def import_key(self, key_data):
+        ''' import the key_data into our keyring '''
+        child_stdout, child_stdin, child_stderr = \
+            self._open_subprocess('--import')
+
+        child_stdin.write(key_data)
+        child_stdin.close()
+
+        # Get the response information
+        result = ImportResult()
+        resp = self._read_response(child_stderr, result)
+
+        return result
+
+    def list_keys(self):
+        ''' list the keys currently in the keyring '''
+        child_stdout, child_stdin, child_stderr = \
+            self._open_subprocess('--list-keys --with-colons')
+        child_stdin.close()
+
+        # TODO: there might be some status thingumy here I should handle...
+
+        # Get the response information
+        result = ListResult()
+        valid_keywords = 'pub uid'.split()
+        while 1:
+            line = child_stdout.readline()
+            if not line:
+                break
+            L = line.strip().split(':')
+            if not L:
+                continue
+            keyword = L[0]
+            if keyword in valid_keywords:
+                getattr(result, keyword)(L)
+
+        return result
+
+    #
+    # ENCRYPTING DATA
+    #
+    def encrypt_file(self, file, recipients):
+        "Encrypt the message read from the file-like object 'file'"
+        args = ['--encrypt --armor']
+        for recipient in recipients:
+            args.append('--recipient %s'%recipient)
+        result = EncryptedMessage()
+        self._handle_gigo(args, file, result)
+        return result
+
+    def encrypt(self, data, recipients):
+        "Encrypt the message contained in the string 'data'"
+        file = StringIO.StringIO(data)
+        return self.encrypt_file(file, recipients)
+
+
+    # Not yet implemented, because I don't need these methods
+    # The methods certainly don't have all the parameters they'd need.
+    def sign(self, data):
+        "Sign the contents of the string 'data'"
+        pass
+
+    def sign_file(self, file):
+        "Sign the contents of the file-like object 'file'"
+        pass
+
+    def decrypt_file(self, file):
+        "Decrypt the message read from the file-like object 'file'"
+        pass
+
+    def decrypt(self, data):
+        "Decrypt the message contained in the string 'data'"
+        pass
+
+##    
+##if __name__ == '__main__':
+##    import sys
+##    if len(sys.argv) == 1:
+##        print 'Usage: GPG.py <signed file>'
+##        sys.exit()
+##
+##    obj = GPGSubprocess()
+##    file = open(sys.argv[1], 'rb')
+##    sig = obj.verify_file( file )
+##    print sig.__dict__
+GPG = Dummy()
+GPG.EncryptedMessage = EncryptedMessage
+GPG.GPGSubprocess = GPGSubprocess
+GPG.ImportResult = ImportResult
+GPG.ListResult = ListResult
+GPG.Signature = Signature
 #!/usr/bin/env python
 ########################################################################
 #
@@ -2442,7 +2865,7 @@ xmlutils.get_xml_tag_strings = get_xml_tag_strings
 
 
 # DO NOT CHANGE
-VERSION="Metalink Checker Version 3.7.1"
+VERSION="Metalink Checker Version 3.7.2"
 
 
 def translate():
@@ -2480,6 +2903,7 @@ def run():
     parser.add_option("--lang", "-l", dest="language", metavar="LANG", help=_("Language preference (ISO-639/3166)"))
     parser.add_option("--country", "-c", dest="country", metavar="LOC", help=_("Two letter country preference (ISO 3166-1 alpha-2)"))
     parser.add_option("--pgp-keys", "-k", dest="pgpdir", metavar="DIR", help=_("Directory with the PGP keys that you trust (default: working directory)"))
+    parser.add_option("--pgp-store", "-p", dest="pgpstore", metavar="FILE", help=_("File with the PGP keys that you trust (default: ~/.gnupg/pubring.gpg)"))
     (options, args) = parser.parse_args()
 
     if options.filevar == None:
@@ -2496,6 +2920,8 @@ def run():
         download.COUNTRY = options.country
     if options.pgpdir != None:
         download.PGP_KEY_DIR = options.pgpdir
+    if options.pgpstore != None:
+        download.PGP_KEY_STORE = options.pgpstore
         
     if options.timeout != None:
         socket.setdefaulttimeout(int(options.timeout))
