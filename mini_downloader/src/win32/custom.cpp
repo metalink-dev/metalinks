beb3b5c092fa98607faaeaf6726b6b9bbc5b0a1b
hampus_wessman <hampus.wessman@gmail.com>
2008-08-11 18:02:43 +0000

    Now supports custom text strings.

--- mini_downloader/src/win32/custom.cpp
+++ mini_downloader/src/win32/custom.cpp
@@ -1,6 +1,8 @@
 #include "custom.h"
 #include "util/tstr.h"
+#include <string>
 
+using namespace std;
 using namespace tstr;
 
 namespace custom {
@@ -8,37 +10,58 @@ namespace custom {
 // The url to download (probably a metalink).
 tstr::tstring get_url()
 {
-    return TSTR("http://www.metalinker.org/samples/OOo/OOo_2.4.1_Win32Intel_install_wJRE_en-US.exe.metalink");
+    // 256 chars long
+    char text[] = "#URL#                                                                                                                                                                                                                                                          #";
+    return s2t(text);
+    //return TSTR("http://www.metalinker.org/samples/OOo/OOo_2.4.1_Win32Intel_install_wJRE_en-US.exe.metalink");
 }
 
 // The filename of the file.
 tstr::tstring get_filename()
 {
-    return TSTR("OOo_2.4.1_Win32Intel_install_wJRE_en-US.exe.metalink");
+    // 100 chars long
+    char text[] = "#FILENAME#                                                                                         #";
+    return s2t(text);
+    //return TSTR("OOo_2.4.1_Win32Intel_install_wJRE_en-US.exe.metalink");
 }
 
 // Welcome msg, to show when the app is started
 tstr::tstring get_welcome_msg()
 {
-    return TSTR("Ready to download OpenOffice.org installer!");
+    // 100 chars long
+    char text[] = "#WELCOME#                                                                                          #";
+    return s2t(text);
+    //return TSTR("Ready to download OpenOffice.org installer!");
 }
 
 // The title of the window.
 tstr::tstring get_title()
 {
-    return TSTR("Metalink Downloader - OpenOffice.org 2.4.0");
+    // 100 chars long
+    char text[] = "#TITLE#                                                                                            #";
+    return TSTR("Metalink Downloader - ") + s2t(text);
+    //return TSTR("Metalink Downloader - OpenOffice.org 2.4.1");
 }
 
 // Should the file be launched, after the download has finished?
 bool get_launch()
 {
-    return true;
+    // 8 chars long
+    char text[] = "#LAUNCH#";
+    if(string(text) == "YES") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // Launch button msg.
 tstr::tstring get_launch_button_msg()
 {
-    return TSTR("Launch installer");
+    // 25 chars long
+    char text[] = "#LAUNCHBTN#             #";
+    return s2t(text);
+    //return TSTR("Launch installer");
 }
 
 }
