5a0da46f2fe9a298b4a410a52f927ec18997425b
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-09-09 21:10:13 +0000

    Untested windows path translation patch

--- metalink/src/metalink.cc
+++ metalink/src/metalink.cc
@@ -60,6 +60,7 @@
 #include "String/String.hh"
 //#define DEBUGLEVEL 3
 #include "Preprocessor/debug.hh"
+#include "Preprocessor/os_win.hh"
 
 #include <cassert>
 #include <sys/stat.h>	//mkdir(2)
@@ -166,16 +167,6 @@ try
 			cerr << "No input files or md5 flags given\nSee: " << argv[0] << " --help\n";
 			return 1;
 		}
-
-
-/*
-		if(variableMap.count("links"))
-		{
-			vector<string> ttmp = variableMap["links"].as< vector<string> >();
-			_foreach(i, ttmp)
-				linkFiles.push_back(*i);
-		}
-*/
 		
 		_foreach(i, options.opt.digests)
 			digests.insert(*i);
@@ -203,192 +194,7 @@ try
 			readMirrors = false;
 		}
   
-  
-  
-  
-  
-  //This one shows the results of multiple instance of the same option, such as --list=1 --list=a --list=b
 
-
-///Old code
-/*
-	po::variables_map variableMap;
-	bool allDigests(false), readMirrors(true), hashList(false);
-	string baseUrl("");
-	string metalinkDescription("");
-	string headerFile("");
-*/
-
-
-
-
-
-  
-/////////Program argument handling
-	
-/*	try
-	{
-		//Parse options
-		po::options_description generalOptions("General options");
-		generalOptions.add_options()
-			("help,h", "Produce a help message")
-			("version", "Print out the name and version")
-			("md5", po::value< vector<string> >(), "Generate metalink from md5sum file(s)")
-			("addpath", po::value< string >(), "Append a path to the mirrors ('/' is not checked)")
-			("headerfile", po::value< string >(), "Include file after the root element declaration.")
-			("nomirrors", "Don't read mirrors from stdin")
-			("hashlist", "List hashes only (implies nomirrors)")
-			("desc", po::value< string >(), "Add metalink description")
-			;
-
-		po::options_description digestOptions("Digest options");
-		digestOptions.add_options()
-			("digest,d", po::value< vector<string> >(), "Include given digest")
-			("mindigests", "Include: md5 sha1")
-			("somedigests", "Include: md5 sha1 ed2k")
-			("alldigests", "Include all possible digests")
-			;
-
-	  po::options_description hiddenOptions("Hidden options");
-  	hiddenOptions.add_options()
-    	("input-file", po::value< vector<string> >(), "Input file(s)")
-	    ;
-    
-  	po::options_description helpOptions;
-	  helpOptions.add(generalOptions).add(digestOptions);
-
-  
-	  po::options_description allOptions;
-  	allOptions.add(generalOptions).add(digestOptions).add(hiddenOptions);
-  
-		po::positional_options_description positional;
-		positional.add("input-file", -1);
-
-		po::store(
-			po::command_line_parser(argc, argv)
-			.options(allOptions)
-			.positional(positional)
-			.run()
-			,variableMap);
-
-		po::notify(variableMap);
-		
-		//Handle options
-		if(variableMap.count("help"))
-		{
-		cout << Globals::programName << " - " << Globals::programDescription << "\n";
-		cout << "Version " << Globals::version[0] << "." << Globals::version[1] << "." << Globals::version[2];
-		cout << ", Copyright (C) 2005 A. Bram Neijt <bneijt@gmail.com>\n";
-		cout << Globals::programName << " comes with ABSOLUTELY NO WARRANTY and is licensed under GPLv2\n";
-		cout << "Usage:\n  " << Globals::programName << " [options] (input files or --md5) < (mirror list) > (metalinkfile)\n";
-		cout << helpOptions << "\n";
-		cout << "Supported algorithms are (-d options):\n"
-			<< "  md4 md5 sha1 sha256 sha384 sha512 rmd160 tiger crc32 ed2k gnunet sha1pieces"
-			<< "\n";
-		cout << "\nMirror lists are single line definitions according to:\n"
-				 << " [location [preference] [type] % ] <mirror base url>\n";
-		cout << "\nExamples:\n";
-		cout << "http://example.com/ as a mirror:\n echo http://example.com | "
-				 << Globals::programName << " -d md5 -d sha1 *\n";
-		cout << "\nhttp://example.com/ as a mirror with preference and location:\n "
-				 << "echo us 10 % http://example.com | " << Globals::programName << " -d md5 -d sha1 *\n";
-		cout << "\nhttp://example.com/ as a mirror with preference only:\n "
-				 << "echo 0 10 % http://example.com | " << Globals::programName << " -d md5 *\n";
-		cout << "\nOnly P2P links:\n "
-				 << Globals::programName << " --nomirrors -d sha1 -d ed2k -d gnunet *\n";
-			return 1;
-		}
-		if(variableMap.count("version"))
-		{
-			cout << Globals::programName << " version "
-					<< Globals::version[0] << "."
-					<< Globals::version[1] << "."
-					<< Globals::version[2] << "\n";
-			return 1;
-		}		
-		//Verify input files
-		if(variableMap.count("input-file") == 0 && variableMap.count("md5") == 0)
-		{
-			cerr << "No input files or md5 flags given\nTry: " << argv[0] << " --help\n";
-			return 1;
-		} 
-
-
-
-
-		//Store input files and digests for later use
-		if(variableMap.count("input-file"))
-		{
-			vector<string> tmp = variableMap["input-file"].as< vector<string> >();
-			_foreach(i, tmp)
-				inputFiles.push_back(*i);
-		}
-
-
-		if(variableMap.count("md5"))
-		{
-			vector<string> ttmp = variableMap["md5"].as< vector<string> >();
-			_foreach(i, ttmp)
-				md5Files.push_back(*i);
-		}
-*/
-/*
-		if(variableMap.count("links"))
-		{
-			vector<string> ttmp = variableMap["links"].as< vector<string> >();
-			_foreach(i, ttmp)
-				linkFiles.push_back(*i);
-		}
-*/
-/*
-		if(variableMap.count("digest"))
-		{
-			vector<string> ttmp = variableMap["digest"].as< vector<string> >();
-			_foreach(i, ttmp)
-				digests.insert(*i);
-		}
-		if(variableMap.count("mindigests"))
-		{
-			digests.insert("md5");
-			digests.insert("sha1");
-		}
-		if(variableMap.count("somedigests"))
-		{
-			digests.insert("md5");
-			digests.insert("sha1");
-			digests.insert("ed2k");
-		}
-
-		if(variableMap.count("addpath") > 0)
-			baseUrl = variableMap["addpath"].as< string >();
-		if(variableMap.count("headerfile") > 0)
-			headerFile = variableMap["headerfile"].as< string >();
-		if(variableMap.count("desc") > 0)
-			metalinkDescription = variableMap["desc"].as< string >();
-		
-		
-		//Simple boolean options
-		allDigests = variableMap.count("alldigests") > 0;
-		readMirrors = variableMap.count("nomirrors") == 0;
-		if(variableMap.count("hashlist"))
-		{
-			if(not (variableMap.count("digest") || allDigests))
-				cerr << "metalink: Warning: No digests given, you probably forgot the -d option." << endl;
-			readMirrors = false;
-			hashList = true;
-		}
-	}
-  catch(exception& e)
-  {
-  	cerr << "error: " << e.what() << endl;
-  	throw;
-  }
-  catch(...)
-  {
-  	cerr << "Exception of unknown type??" << endl;
-		throw;
-  }
-*/  
 	//Read paths from stdin
 	MirrorList const mirrorList(cin, baseUrl, readMirrors);
 	
@@ -416,27 +222,13 @@ try
 	_foreach(it, inputFiles)
 	{
 		String filename(*it);
-
+#ifdef OS_WIN
+    filename = filename.translated('\\', '/');
+#endif
 		//Silently skip metalink files
 		if(filename.endsIn(Globals::metalinkExtension))
 			continue;
-/*
 
-		boost::filesystem::path targetFile(filename, boost::filesystem::native);
-
-		if(!boost::filesystem::exists(targetFile))
-		{
-			cerr << "Can't find '" << filename << "'\n";
-			continue;
-		}
-		
-
-		if(boost::filesystem::is_directory( targetFile ))
-		{
-			cerr << "Skipping directory '" << filename << "'\n";
-			continue;
-		}
-*/		
 		ifstream file(filename.c_str(), ios::binary);
 		if(!file.is_open())
 		{
@@ -562,7 +354,6 @@ try
 
 			//Either make pieces a special name or add a new system for piece based hashing
 			record.addVerification((*hp)->xml());
-//			record.addVerification((*hp)->name(), (*hp)->value());
 			
 			//Add P2P specials
 			if((*hp)->name() == "sha1")
@@ -583,10 +374,6 @@ try
 	
 	return 0;
 }
-/*catch(const boost::program_options::unknown_option &e)
-{
-	cerr << e.what() << endl;
-}*/
 catch(std::string const &e)
 {
 	cerr << "Exiting with ERRORS: " << e << endl;	
