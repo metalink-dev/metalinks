ef2a8d91d6cb923b2219120e5ed43f65be9af3bf
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-10-21 00:09:07 +0000

    added debian package build files

---
+++ checker/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the metalink-checker package
+#
+0 4	* * *	root	metalink-checker_maintenance
---
+++ checker/debian/metalink-checker-default.ex
@@ -0,0 +1,10 @@
+# Defaults for metalink-checker initscript
+# sourced by /etc/init.d/metalink-checker
+# installed at /etc/default/metalink-checker by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
