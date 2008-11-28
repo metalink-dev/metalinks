b9e76ff63c34952cbb5a4ae9f818f68613891e1f
bneijt <bneijt@gmail.com>
2008-11-28 15:14:15 +0000

    0.3.5 packaging cleanup release

--- metalink/package.sh
+++ metalink/package.sh
@@ -20,4 +20,4 @@
 sh bootstrap
 ccbuild --nodefargs --args "-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" --addres src/ccResolutions makefile src/metalink.cc > Makefile.metalink
 ccbuild --nodefargs --args "-D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE" --addres src/ccResolutions aap src/metalink.cc > metalink.aap
-
+docbook-to-man doc/metalink.sgml > doc/metalink.1
