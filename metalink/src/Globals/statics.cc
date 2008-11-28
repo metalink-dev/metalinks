ecbe7f00fcb66cd44f2de46fd51eb0c127eb4637
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2008-11-28 15:14:15 +0000

    0.3.5 packaging cleanup release

--- metalink/src/Globals/statics.cc
+++ metalink/src/Globals/statics.cc
@@ -27,7 +27,7 @@
 
 std::string const Globals::programName("metalink");
 std::string const Globals::programDescription("Metalink XML generator");
-unsigned const Globals::version[] = {0,3,4};
+unsigned const Globals::version[] = {0,3,5};
 
 std::string Globals::metalinkExtension(".metalinks");
 char const Globals::dirSep('/');
