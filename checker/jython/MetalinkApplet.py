74c983d4c682e56ee4f0f92a90d17febafe0877c
nabber00 <nabber00@gmail.com>
2009-06-06 22:44:20 +0000

    Reworked for jython applet support

--- checker/jython/MetalinkApplet.py
+++
@@ -1,7 +0,0 @@
-import metalink
-
-from java.applet import Applet
-
-class MetalinkApplet(Applet):
-    def paint(self, g):
-        g.drawString("Hello from Jython!", 20, 30)
--- checker/jython/jythonapplet.bat
+++
@@ -1,1 +0,0 @@
-c:\jython2.2.1\jythonc.bat --core --deep --jar MetalinkApplet.jar MetalinkApplet.py
