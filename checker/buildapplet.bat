9f1017f0e9c1270d00d8d0364963649492a743b8
nabber00 <nabber00@gmail.com>
2009-06-07 23:32:27 +0000

    updated build script with two modes, quick and optimized

--- checker/buildapplet.bat
+++ checker/buildapplet.bat
@@ -6,33 +6,10 @@ set APPNAME=DLApplet
 call sample_setenv.bat
 call setenv.bat
 
+echo Cleaning up old files...
 call %JYTHON_HOME%\jython.bat setup.py clean
 
 mkdir %BUILD_DIR%
-
-echo Repackaging jython with only needed classes
-mkdir %BUILD_DIR%\jython
-cd %BUILD_DIR%\jython
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar com/sun
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/antlr
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/compiler
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/constantine
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/core
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/expose
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/modules
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/objectweb
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/posix
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/util
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/Version.class
-%JAVA_HOME%\bin\jar.exe xvf %JYTHON_HOME%\jython.jar org/python/version.properties
-
-%JAVA_HOME%\bin\jar.exe cvf jython.jar *
-move jython.jar ..\.
-cd ..
-rmdir /S /Q jython
-cd ..
-
-pause
 
 cd jyinterface
 %JAVA_HOME%\bin\javac.exe -Xlint:unchecked -classpath %JYTHON_HOME%\jython.jar Download.java Downloader.java JythonFactory.java ProgressRenderer.java DownloadTableModel.java DownloadManager.java DLApplet.java
@@ -40,9 +17,62 @@ mkdir ..\%BUILD_DIR%\jyinterface
 move *.class ..\%BUILD_DIR%\jyinterface
 cd ..
 
+:: If opt parameter is specified, use optimized mode
+IF "%1"=="opt" GOTO opt
+
+:quick
+echo Doing a quick build...
+xcopy /S /I /Q %JYTHON_HOME%\Lib\* %BUILD_DIR%\.
+copy %JYTHON_HOME%\jython.jar %BUILD_DIR%\.
+
+copy JDownload.py %BUILD_DIR%\.
+copy JDownloader.py %BUILD_DIR%\.
+copy download.py %BUILD_DIR%\.
+copy xmlutils.py %BUILD_DIR%\.
+
+GOTO jar
+
+:jar
+echo Compressing into jar file...
+cd %BUILD_DIR%
+echo Class-Path: jython.jar > manifest.txt
+%JAVA_HOME%\bin\jar.exe cfme %APPNAME%.jar manifest.txt jyinterface.DownloadManager jyinterface xml/*.py xml/parsers/*.py encodings/*.py *.py
+
+echo Signing applet...
+%JAVA_HOME%\bin\jarsigner.exe %APPNAME%.jar %APPNAME%
+
+cd ..
+
+GOTO end
+
+:opt
+echo Repackaging jython with only needed classes...
+mkdir %BUILD_DIR%\jython
+cd %BUILD_DIR%\jython
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar com/sun
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/antlr
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/apache/xerces
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/compiler
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/constantine
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/core
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/expose
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/modules
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/objectweb
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/posix
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/util
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/Version.class
+%JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/version.properties
+
+%JAVA_HOME%\bin\jar.exe cf jython.jar *
+move jython.jar ..\.
+cd ..
+rmdir /S /Q jython
+cd ..
+
+echo Doing an optimized build...
 REM copy %JYTHON_HOME%\Lib\* %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\urllib2.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\thread.py %BUILD_DIR%\.
+REM copy %JYTHON_HOME%\Lib\thread.py %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\socket.py %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\os.py %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\md5.py %BUILD_DIR%\.
@@ -89,20 +119,14 @@ copy %JYTHON_HOME%\Lib\copy.py %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\BaseHTTPServer.py %BUILD_DIR%\.
 copy %JYTHON_HOME%\Lib\SocketServer.py %BUILD_DIR%\.
 
-xcopy /S /I %JYTHON_HOME%\Lib\xml %BUILD_DIR%\xml
-xcopy /S /I %JYTHON_HOME%\Lib\encodings %BUILD_DIR%\encodings
+xcopy /S /I /Q %JYTHON_HOME%\Lib\xml %BUILD_DIR%\xml
+xcopy /S /I /Q %JYTHON_HOME%\Lib\encodings %BUILD_DIR%\encodings
 
 copy JDownload.py %BUILD_DIR%\.
 copy JDownloader.py %BUILD_DIR%\.
 copy download.py %BUILD_DIR%\.
 copy xmlutils.py %BUILD_DIR%\.
 
-cd %BUILD_DIR%
-echo Class-Path: jython.jar > manifest.txt
-%JAVA_HOME%\bin\jar.exe cvfme %APPNAME%.jar manifest.txt jyinterface.DownloadManager jyinterface xml/*.py xml/parsers/*.py encodings/*.py *.py
+GOTO jar
 
-REM %JAVA_HOME%\bin\java.exe -jar applet.jar
-
-%JAVA_HOME%\bin\jarsigner.exe %APPNAME%.jar %APPNAME%
-
-cd ..
+:end
