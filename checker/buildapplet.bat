091b1e291738837d40c3629178a319cad818824c
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2009-06-07 00:20:16 +0000

    Added Java applet capability, still not working yet though

--- checker/buildapplet.bat
+++ checker/buildapplet.bat
@@ -1,18 +1,17 @@
-REM set JYTHON_HOME="..\"
 @ECHO ON
 
 set JYTHON_HOME="c:\jython2.5rc3"
 set JAVA_HOME="C:\Program Files\Java\jdk1.6.0_12"
 set BUILD_DIR=build
 
-%JYTHON_HOME%\jython.bat setup.py clean
+call %JYTHON_HOME%\jython.bat setup.py clean
 
 mkdir %BUILD_DIR%
 
 copy %JYTHON_HOME%\jython.jar %BUILD_DIR%\.
 
 cd jyinterface
-%JAVA_HOME%\bin\javac.exe -classpath %JYTHON_HOME%\jython.jar Download.java JythonFactory.java Main.java
+%JAVA_HOME%\bin\javac.exe -classpath %JYTHON_HOME%\jython.jar Download.java JythonFactory.java Main.java DLApplet.java
 mkdir ..\%BUILD_DIR%\jyinterface
 mv *.class ..\%BUILD_DIR%\jyinterface
 cd ..
@@ -20,12 +19,11 @@ cd ..
 xcopy /S %JYTHON_HOME%\Lib\* %BUILD_DIR%\.
 
 copy download.py %BUILD_DIR%\.
-REM copy GPG.py %BUILD_DIR%\.
 copy xmlutils.py %BUILD_DIR%\.
 
 cd %BUILD_DIR%
 echo Class-Path: jython.jar > manifest.txt
-%JAVA_HOME%\bin\jar.exe cvfme applet.jar manifest.txt jyinterface.Main *
+%JAVA_HOME%\bin\jar.exe cvfme DLApplet.jar manifest.txt jyinterface.Main *
 
 REM %JAVA_HOME%\bin\java.exe -jar applet.jar
 
