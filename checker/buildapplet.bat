9d6930e2e1c1a2793e8b6b177a3e8721fbe9d3dd
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2009-06-08 11:46:57 +0000

    Added better compression for .jar files with 7-zip

--- checker/buildapplet.bat
+++ checker/buildapplet.bat
@@ -1,4 +1,4 @@
-@ECHO ON
+@ECHO OFF
 
 set BUILD_DIR=build
 set APPNAME=DLApplet
@@ -16,39 +16,58 @@ cd jyinterface
 mkdir ..\%BUILD_DIR%\jyinterface
 move *.class ..\%BUILD_DIR%\jyinterface
 cd ..
+cd %BUILD_DIR%
 
 :: If opt parameter is specified, use optimized mode
 IF "%1"=="opt" GOTO opt
 
 :quick
-echo Doing a quick build...
-xcopy /S /I /Q %JYTHON_HOME%\Lib\* %BUILD_DIR%\.
-copy %JYTHON_HOME%\jython.jar %BUILD_DIR%\.
+echo Doing a quick build... (for a distributable build you should use the "opt" option to optimize)
+xcopy /S /I /Q %JYTHON_HOME%\Lib\* .
+
+copy %JYTHON_HOME%\jython.jar .
+
+GOTO jar
+
+:jar
+
+cd ..
 
 copy JDownload.py %BUILD_DIR%\.
 copy JDownloader.py %BUILD_DIR%\.
 copy download.py %BUILD_DIR%\.
 copy xmlutils.py %BUILD_DIR%\.
 
-GOTO jar
+echo Compressing into jar files...
 
-:jar
-echo Compressing into jar file...
 cd %BUILD_DIR%
+
 echo Class-Path: jython.jar > manifest.txt
-%JAVA_HOME%\bin\jar.exe cfme %APPNAME%.jar manifest.txt jyinterface.DownloadManager jyinterface xml/*.py xml/parsers/*.py encodings/*.py *.py
+%JAVA_HOME%\bin\jar.exe cfme0 %APPNAME%.jar manifest.txt jyinterface.DownloadManager jyinterface xml/*.py xml/parsers/*.py encodings/*.py *.py
 
 echo Signing applet...
-%JAVA_HOME%\bin\jarsigner.exe %APPNAME%.jar %APPNAME%
 
+%JAVA_HOME%\bin\jarsigner.exe %APPNAME%.jar %APPNAME%
+%JAVA_HOME%\bin\jarsigner.exe jython.jar %APPNAME%
+
+IF "%1"=="" GOTO end
+
+echo Recompressing...
+
+for %%F in (jython DLApplet) do (
+%ZIP% x -tzip %%F.jar -otemp
+cd temp
+%ZIP% a -tzip -mx=9 ../%%F.jar *
 cd ..
+rmdir /S /Q temp
+)
 
 GOTO end
 
 :opt
 echo Repackaging jython with only needed classes...
-mkdir %BUILD_DIR%\jython
-cd %BUILD_DIR%\jython
+mkdir jython
+cd jython
 %JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar com/sun
 %JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/antlr
 %JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/apache/xerces
@@ -63,70 +82,67 @@ cd %BUILD_DIR%\jython
 %JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/Version.class
 %JAVA_HOME%\bin\jar.exe xf %JYTHON_HOME%\jython.jar org/python/version.properties
 
-%JAVA_HOME%\bin\jar.exe cf jython.jar *
+%JAVA_HOME%\bin\jar.exe cf0 jython.jar *
 move jython.jar ..\.
 cd ..
-rmdir /S /Q jython
-cd ..
 
-echo Doing an optimized build...
+:: import standard .py files
+
+echo Doing an optimized .py build...
 REM copy %JYTHON_HOME%\Lib\* %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\urllib2.py %BUILD_DIR%\.
+copy %JYTHON_HOME%\Lib\urllib2.py .
 REM copy %JYTHON_HOME%\Lib\thread.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\socket.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\os.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\md5.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\sha.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\re.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\math.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\time.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\urlparse.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\hashlib.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\locale.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\threading.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\ftplib.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\httplib.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\base64.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\sys.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\gettext.py %BUILD_DIR%\.
+copy %JYTHON_HOME%\Lib\socket.py .
+copy %JYTHON_HOME%\Lib\os.py .
+copy %JYTHON_HOME%\Lib\md5.py .
+copy %JYTHON_HOME%\Lib\sha.py .
+copy %JYTHON_HOME%\Lib\re.py .
+copy %JYTHON_HOME%\Lib\math.py .
+copy %JYTHON_HOME%\Lib\time.py .
+copy %JYTHON_HOME%\Lib\urlparse.py .
+copy %JYTHON_HOME%\Lib\hashlib.py .
+copy %JYTHON_HOME%\Lib\locale.py .
+copy %JYTHON_HOME%\Lib\threading.py .
+copy %JYTHON_HOME%\Lib\ftplib.py .
+copy %JYTHON_HOME%\Lib\httplib.py .
+copy %JYTHON_HOME%\Lib\base64.py .
+copy %JYTHON_HOME%\Lib\sys.py .
+copy %JYTHON_HOME%\Lib\gettext.py .
 
-copy %JYTHON_HOME%\Lib\string.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\sre_compile.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\sre_constants.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\sre_parse.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\copy_reg.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\types.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\weakref.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\UserDict.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\traceback.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\linecache.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\stat.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\ntpath.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\atexit.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\mimetools.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\rfc822.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\tempfile.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\random.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\warnings.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\posixpath.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\bisect.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\urllib.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\codecs.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\gzip.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\zlib.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\StringIO.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\copy.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\BaseHTTPServer.py %BUILD_DIR%\.
-copy %JYTHON_HOME%\Lib\SocketServer.py %BUILD_DIR%\.
+copy %JYTHON_HOME%\Lib\string.py .
+copy %JYTHON_HOME%\Lib\sre_compile.py .
+copy %JYTHON_HOME%\Lib\sre_constants.py .
+copy %JYTHON_HOME%\Lib\sre_parse.py .
+copy %JYTHON_HOME%\Lib\copy_reg.py .
+copy %JYTHON_HOME%\Lib\types.py .
+copy %JYTHON_HOME%\Lib\weakref.py .
+copy %JYTHON_HOME%\Lib\UserDict.py .
+copy %JYTHON_HOME%\Lib\traceback.py .
+copy %JYTHON_HOME%\Lib\linecache.py .
+copy %JYTHON_HOME%\Lib\stat.py .
+copy %JYTHON_HOME%\Lib\ntpath.py .
+copy %JYTHON_HOME%\Lib\atexit.py .
+copy %JYTHON_HOME%\Lib\mimetools.py .
+copy %JYTHON_HOME%\Lib\rfc822.py .
+copy %JYTHON_HOME%\Lib\tempfile.py .
+copy %JYTHON_HOME%\Lib\random.py .
+copy %JYTHON_HOME%\Lib\warnings.py .
+copy %JYTHON_HOME%\Lib\posixpath.py .
+copy %JYTHON_HOME%\Lib\bisect.py .
+copy %JYTHON_HOME%\Lib\urllib.py .
+copy %JYTHON_HOME%\Lib\codecs.py .
+copy %JYTHON_HOME%\Lib\gzip.py .
+copy %JYTHON_HOME%\Lib\zlib.py .
+copy %JYTHON_HOME%\Lib\StringIO.py .
+copy %JYTHON_HOME%\Lib\copy.py .
+copy %JYTHON_HOME%\Lib\BaseHTTPServer.py .
+copy %JYTHON_HOME%\Lib\SocketServer.py .
 
-xcopy /S /I /Q %JYTHON_HOME%\Lib\xml %BUILD_DIR%\xml
-xcopy /S /I /Q %JYTHON_HOME%\Lib\encodings %BUILD_DIR%\encodings
-
-copy JDownload.py %BUILD_DIR%\.
-copy JDownloader.py %BUILD_DIR%\.
-copy download.py %BUILD_DIR%\.
-copy xmlutils.py %BUILD_DIR%\.
+xcopy /S /I /Q %JYTHON_HOME%\Lib\xml xml
+xcopy /S /I /Q %JYTHON_HOME%\Lib\encodings encodings
 
 GOTO jar
 
 :end
+
+cd ..
