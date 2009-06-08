b3203e84d28bab0ce271391376cb402010f04561
nabber00 <nabber00@gmail.com>
2009-06-08 08:40:07 +0000

    fixed factory to use PYTHONPATH imports instead of file paths

--- checker/jyinterface/JythonFactory.java
+++ checker/jyinterface/JythonFactory.java
@@ -19,15 +19,19 @@ public class JythonFactory {
               
        Object javaInt = null;
        PythonInterpreter interpreter = new PythonInterpreter();
-       interpreter.execfile(pathToJythonModule);
-       String tempName = pathToJythonModule.substring(pathToJythonModule.lastIndexOf("/")+1);
-       tempName = tempName.substring(0, tempName.indexOf("."));
-       System.out.println(tempName);
+       //interpreter.execfile(pathToJythonModule);
+       interpreter.exec("import " + pathToJythonModule);
+       //String tempName = pathToJythonModule.substring(pathToJythonModule.lastIndexOf("/")+1);
+       String tempName = pathToJythonModule.substring(pathToJythonModule.lastIndexOf(".")+1);
+       //tempName = tempName.substring(0, tempName.indexOf("."));
+       //System.out.println(tempName);
        String instanceName = tempName.toLowerCase();
        String javaClassName = tempName.substring(0,1).toUpperCase() +
                            tempName.substring(1);
-       String objectDef = "=" + javaClassName + "()";
+       //String objectDef = "=" + javaClassName + "()";
+       String objectDef = "=" + pathToJythonModule + "." + javaClassName + "()";
        interpreter.exec(instanceName + objectDef);
+       //System.out.println(instanceName + objectDef);
         try {
            Class JavaInterface = Class.forName(interfaceName);
            javaInt = 
