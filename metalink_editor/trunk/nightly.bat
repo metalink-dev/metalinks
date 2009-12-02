e0453ff8150fd67e9ca899919bdddd46b31074c6
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2009-12-02 07:34:23 +0000

    bugfix

--- metalink_editor/trunk/nightly.bat
+++ metalink_editor/trunk/nightly.bat
@@ -4,10 +4,10 @@ call setenv.bat
 mkdir "%BUILDS%"
 
 call sdist.bat
-xcopy /Y "dist\*.zip" "%BUILDS%\."
-move "%BUILDS%\*.zip" "%BUILDS%\%DATE%_Metalink_Editor.zip"
+xcopy /Y "dist\Metalink Editor-*.zip" "%BUILDS%\."
+move "%BUILDS%\Metalink Editor-*.zip" "%BUILDS%\%DATE%_Metalink_Editor.zip"
 call make_installer.bat
-xcopy /Y "*-win32.zip" "%BUILDS%\."
-move "%BUILDS%\*-win32.zip" "%BUILDS%\%DATE%_Metalink_Editor-win32.zip"
+xcopy /Y "Metalink Editor-*-win32.zip" "%BUILDS%\."
+move "%BUILDS%\Metalink Editor-*-win32.zip" "%BUILDS%\%DATE%_Metalink_Editor-win32.zip"
 xcopy /Y "Metalink Editor-*.exe" "%BUILDS%\."
 move "%BUILDS%\Metalink Editor-*.exe" "%BUILDS%\%DATE%_Metalink_Editor.exe"
