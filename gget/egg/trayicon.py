09edd460bb434931f7638efdd0d60f538c89aeb8
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2010-01-22 04:03:55 +0000

    trayicon now appears, doesn't do anything

--- gget/egg/trayicon.py
+++ gget/egg/trayicon.py
@@ -1,9 +1,40 @@
-class TrayIcon:
+import gtk
+import gtk.gdk
+
+class TrayIcon(gtk.StatusIcon):
     def __init__(self, name):
-        pass
+        gtk.StatusIcon.__init__(self)
+    
+    def add(self, eventbox):
+        self.eb = eventbox
+        image = eventbox.get_children()[0]
+        icon_name = image.get_icon_name()[0] 
+        self.set_from_icon_name(icon_name)
+        self.connect("activate", self.activate)
+        self.connect("popup_menu", self.popup)
+
+    def activate(self, widget):
+        window = self.eb.get_root_window()
+        # this doesn't seem to work for some reason
+        if window.is_visible():
+            #print "hiding"
+            window.hide()
+        else:
+            #print "showing"
+            window.show()
+        #print self.eb.translate_coordinates(self.eb.get_toplevel(), 0, 0)
+        #event = gtk.gdk.Event(gtk.gdk.BUTTON_PRESS)
+        #event.button = 1
+        #event.x = 0
+        #event.y = 0
+        #gtk.main_do_event(event)
+        #print self.eb.get_size_request()
+        #self.eb.event(event)
         
-    def add(self, obj):
-        return
+
+    def popup(self, widget, two, three):
+        #print widget,two, three
+        pass
         
     def show_all(self):
         return
