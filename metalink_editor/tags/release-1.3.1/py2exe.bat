cefc5ba1c39ffb2bd6acee37fe82001badae428d
nabber00 <nabber00@gmail.com>
2010-03-14 09:29:57 +0000

    tagging 1.3.1 release

---
+++ metalink_editor/tags/release-1.3.1/py2exe.bat
@@ -0,0 +1,15 @@
+rem *** Used to create a Python exe 
+
+call sample_setenv.bat
+call setenv.bat
+
+SET PYTHONPATH=%PYTHONDIR%\python.exe
+
+%PYTHONPATH% setup.py clean
+
+rem ***** create the exe
+%PYTHONPATH% -OO setup.py py2exe 
+
+%PYTHONPATH% setup.py zip
+
+echo "done..."
