1a61cd9e5ae53dfb154618d9aea18b45f5930fac
nabber00 <nabber00@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2010-04-11 01:50:31 +0000

    bugfix

--- gget/pynotify.py
+++ gget/pynotify.py
@@ -1,5 +1,10 @@
-def Notification(title, text):
-    pass
+class Notification:
+    def __init__(self, title, text):
+        pass
+        
+    def set_icon_from_pixbuf(self, pixbuf):
+        pass
+    
     
 def init(name):
     pass
