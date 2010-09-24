b17be9aabbe0d33b9e95a400f6a5980f00321600
nabber00 <nabber00@gmail.com>
2010-09-24 02:14:18 +0000

    updated build script

--- checker/silverlight/build.bat
+++ checker/silverlight/build.bat
@@ -1,3 +1,17 @@
-Chiron /d:app /z:app.xap
+SET PYTHONLIB=c:\python26\lib
+
+xcopy /Y %PYTHONLIB%\*.py src\.
+
+Chiron /d:src /z:app.xap
+
+del app.xap
+
+cd src
+
+"C:\Program Files\7-Zip\7z.exe" a -r -tzip -xr!*.svn* ..\app.xap .
+
+cd ..
+
+echo Build complete.
 
 pause
