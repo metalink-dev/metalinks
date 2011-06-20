388cb2ee0c23a51aa5290687a34abf8d1b80e932
bneijt <bneijt@84cd5247-9d1f-0410-ad08-ff3d2110de5e>
2011-06-20 09:36:38 +0000

    Remove the minormirror directory
    Minormirror has not seen any development for
    to long and there is nolonger any support for it.
    
    Cleaning it up.

--- minormirror/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-define(MM_HTTP_ROOT, 'http://example.com/');//This should be configured to a complete http:// root, ending in /
-define(MM_PROJECTNAME, 'ExampleProject');
-define(MM_DOWNLOADS_DIR, 'downloads/');//Download directory, with slash.
-define(MM_META_DIR, 'meta/');//Meta data directory, with slash.
-
-$MM_HASHES = explode(' ', 'md5 sha1');//Supported digests.
-$MM_USERS = array('admin' => 'admin');//User password information.
-
